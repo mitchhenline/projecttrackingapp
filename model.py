cf745e1a3856a380a7d8a51e1ee4dcca07fed32c
Mitch Henline <rmhenline@gmail.com>
2022-12-14 18:52:34 -0700

    three complete

--- model.py
+++ model.py
@@ -12,9 +12,12 @@ class User(db.Model):
     username = db.Column(db.String(255), unique=True, nullable = False)
     password = db.Column(db.String(255), nullable = False)
 
+    teams = db.relationship("Team", backref = "user", lazy = True)
+
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
 
 class Team(db.Model):
 
@@ -23,6 +26,8 @@ class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     team_name = db.Column(db.String(255), unique = True, nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+
+    projects = db.relationship("Project", backref = "teams", lazy = True)
 
     def __init__(self, team_name, user_id):
         self.team_name = team_name
@@ -37,6 +42,8 @@ class Project(db.Model):
     description = db.Column(db.String(255), nullable = True)
     completed = db.Column(db.Boolean, default = False)
     team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable = False)
+
+   
 
     def __init__(self, project_name, description, completed, team_id):
         self.project_name = project_name
